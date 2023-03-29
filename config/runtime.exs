120cf726df82ee8b797da1d6d6bb06bf59d6bcfd
Damon Janis <damonvjanis@gmail.com>
2023-03-29 07:01:33 -0600

    Better runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,10 +1,11 @@
 import Config
 
-# Set ports based on environment variables in the release,
-# fall back to ports defined in config (for dev and test)
-http_default = Application.get_env(:my_app, MyAppWeb.Endpoint)[:http][:port] || 4000
-https_default = Application.get_env(:my_app, MyAppWeb.Endpoint)[:https][:port] || 4040
+# Set ports based on environment variables in the release
+http_port = System.get_env("HTTP_PORT")
+https_port = System.get_env("HTTPS_PORT")
 
-config :my_app, MyAppWeb.Endpoint,
-  http: [port: String.to_integer(System.get_env("HTTP_PORT", http_default))],
-  https: [port: String.to_integer(System.get_env("HTTPS_PORT", https_default))]
+if http_port && https_port do
+  config :my_app, MyAppWeb.Endpoint,
+    http: [port: String.to_integer(http_port)],
+    https: [port: String.to_integer(https_default)]
+end
